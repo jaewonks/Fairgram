0e83b9e2a7e70bdd4fc4f82df1e48079b018df0c
Jaewon Kim <jaewon@Jaewons-MBP.cust.communityfibre.co.uk>
2020-10-03 12:05:15 +0100

    Settings

--- src/server.js
+++ src/server.js
@@ -1,1 +1,19 @@
-console.log("ciao Fabio")
+require('dotenv').config
+import { GraphQLServer } from 'graphql-yoga'
+
+const PORT = process.env.PORT || 4000
+const typeDefs = `
+    type Query {
+        hello(name: String): String!
+    }
+`
+
+const resolvers = {
+    Query: {
+        hello: () => 'Hello World'
+      },
+    }
+const server = new GraphQLServer({typeDefs, resolvers})
+
+server.start({port: PORT}, () => console.log(`Server running on http://localhost:${PORT}`))
+//dotenv config에서 포트를 읽어오도록 할 수있다.

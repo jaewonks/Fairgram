73423ef951ac74a98c4adc4f6d6a56f122181e34
Jaewon Kim <jaewon@Jaewons-MBP.cust.communityfibre.co.uk>
2020-11-14 17:13:23 +0000

    Prisma model setup

--- src/schema.js
+++ src/schema.js
@@ -2,7 +2,7 @@ import path from "path";
 import { makeExecutableSchema } from "graphql-tools";
 import { fileLoader, mergeResolvers, mergeTypes } from "merge-graphql-schemas";
 
-const allTypes = fileLoader(path.join(__dirname, "/api/**/*.graphql"));
+const allTypes = fileLoader(path.join(__dirname, "/api/**/*.graphql")); // api폴더 밑의 모든 폴더에 속해있고, 확장자가 .graphql인 모든 파일
 const allResolvers = fileLoader(path.join(__dirname, "/api/**/*.js"));
 
 const schema = makeExecutableSchema({
--- src/server.js
+++ src/server.js
@@ -3,10 +3,10 @@ import { GraphQLServer } from 'graphql-yoga'
 import logger from 'morgan'
 import schema from './schema'
 
-
 const PORT = process.env.PORT || 4000
 const server = new GraphQLServer({ schema })
 
+// GraphQL 에는 express 서버가 내장되어 있다.
 server.express.use(logger("dev"))
 server.start({port: PORT}, () => console.log(`Server running on http://localhost:${PORT}`))
 //dotenv config에서 포트를 읽어오도록 할 수있다.
